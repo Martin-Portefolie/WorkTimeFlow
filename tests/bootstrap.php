f887d13976e497e3f331379870a934428a5b83cc
BMuuh <bmuuh@BMuuhs-MacBook-Pro.local>
2025-08-24 13:40:18 +0200

    chore(recipes): adopt new PHPUnit bootstrap (Symfony recipe)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,10 @@
 <?php
 
 require dirname(__DIR__).'/vendor/autoload.php';
+<<<<<<< ours
+=======
+
+if (method_exists(Dotenv::class, 'bootEnv')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+}
+>>>>>>> theirs
